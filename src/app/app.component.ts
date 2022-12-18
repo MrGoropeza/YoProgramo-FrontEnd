6eec3198c20afdae2df8699e8352c81c71a85762
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-17 22:18:06 -0300

    add: config inicial de primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PrimeNGConfig } from 'primeng/api';
+import { esLanguage } from 'src/core/consts/esLanguage';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'YoProgramo-FrontEnd';
+export class AppComponent implements OnInit{
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+        this.primengConfig.setTranslation(esLanguage);
+    }
+  
 }
