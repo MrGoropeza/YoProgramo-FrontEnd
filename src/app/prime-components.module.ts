a27440fd799d4bec70baf5403ff8ce0f7ffe9577
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-18 21:45:17 -0300

    add: modulos boton, overlaypanel y dialog en primeComponents

--- src/app/prime-components.module.ts
+++ src/app/prime-components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { TerminalModule } from 'primeng/terminal';
+import { ButtonModule } from 'primeng/button';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
-  exports: [TerminalModule],
+  exports: [TerminalModule, ButtonModule, OverlayPanelModule, DialogModule],
 })
 export class PrimeComponentsModule {}
