fe494546c8ef0038409fae4088f8452583d0d225
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-20 03:38:05 -0300

    update: terminal module ahora es para exportar el componente

--- src/app/modules/secured/terminal/terminal.module.ts
+++ src/app/modules/secured/terminal/terminal.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TerminalComponent } from './terminal.component';
-import { RouterModule, Routes } from '@angular/router';
 import { PrimeComponentsModule } from 'src/app/prime-components.module';
 import { TerminalService } from 'primeng/terminal';
 import { UserTerminalService } from 'src/app/project/services/terminal.service';
 import { OverlayTerminalComponent } from './components/overlay-terminal/overlay-terminal.component';
-
-const routes: Routes = [
-  {
-    path: "",
-    component: OverlayTerminalComponent
-  }
-];
 
 @NgModule({
   declarations: [
@@ -20,13 +11,14 @@ const routes: Routes = [
     OverlayTerminalComponent
   ],
   imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
     PrimeComponentsModule 
   ],
   providers: [
     TerminalService,
     UserTerminalService
+  ],
+  exports: [
+    OverlayTerminalComponent,
   ]
 })
 export class TerminalModule { }
