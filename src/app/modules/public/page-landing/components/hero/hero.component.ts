b724a18e70da090d245b41a004518cc7831a755e
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-20 21:59:35 -0300

    add: link a las paginas de ucasal, siltium y mi linkedin

--- src/app/modules/public/page-landing/components/hero/hero.component.ts
+++ src/app/modules/public/page-landing/components/hero/hero.component.ts
@@ -12,4 +12,15 @@ export class HeroComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  goToPersonalPage(){
+    window.open("https://www.linkedin.com/in/gonzalo-oropeza-6170a717b/", "_blank");
+  }
+
+  goToCareer(){
+    window.open("https://www.ucasal.edu.ar/carrera/nivel/carreras-grado/ingenieria-informatica/", "_blank");
+  }
+
+  goToWork(){
+    window.open("https://www.linkedin.com/company/siltium/", "_blank");
+  }
 }
