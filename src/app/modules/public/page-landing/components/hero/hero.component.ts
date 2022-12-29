f8038a938a5e48af2c226615734ca15c08c8ffad
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-29 05:57:52 -0300

    remove: metodos para redireccionar al usuario

--- src/app/modules/public/page-landing/components/hero/hero.component.ts
+++ src/app/modules/public/page-landing/components/hero/hero.component.ts
@@ -20,16 +20,4 @@ export class HeroComponent implements OnInit {
   ngOnInit(): void {
     this.aboutMeInfo$ = this.store.select(selectAboutMeInfo);
   }
-
-  goToPersonalPage(){
-    window.open("https://www.linkedin.com/in/gonzalo-oropeza-6170a717b/", "_blank");
-  }
-
-  goToCareer(){
-    window.open("https://www.ucasal.edu.ar/carrera/nivel/carreras-grado/ingenieria-informatica/", "_blank");
-  }
-
-  goToWork(){
-    window.open("https://www.linkedin.com/company/siltium/", "_blank");
-  }
 }
