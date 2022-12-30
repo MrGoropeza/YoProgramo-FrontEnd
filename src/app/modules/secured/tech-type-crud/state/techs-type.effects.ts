99923a98d6cee31b63931ea8daf05fa575a3dbd1
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-30 00:44:21 -0300

    update: los tipos de tecnologias vuelven a cargar luego de cerrar el CRUD

--- src/app/modules/secured/tech-type-crud/state/techs-type.effects.ts
+++ src/app/modules/secured/tech-type-crud/state/techs-type.effects.ts
@@ -7,15 +7,15 @@ import { TechnologiesService } from 'src/app/project/services/technologies.servi
 import { TechTypeCrudComponent } from '../tech-type-crud.component';
 import { TechTypeFormComponent } from '../tech-type-form/tech-type-form.component';
 import * as TechTypeActions from './techs-type.actions';
+import * as InicioActions from '../../../public/page-landing/state/inicio.actions';
 
 @Injectable()
 export class TechTypeEffects {
-
   constructor(
     private actions$: Actions,
     private techService: TechnologiesService,
     private dialogService: DialogService,
-    private messageService: MessageService,
+    private messageService: MessageService
   ) {}
 
   openTechTypes$ = createEffect(
@@ -33,15 +33,12 @@ export class TechTypeEffects {
     { dispatch: false }
   );
 
-  closeTechTypes$ = createEffect(
-    () => {
-      return this.actions$.pipe(
-        ofType(TechTypeActions.closeTechsTypes),
-        map(() => null)
-      );
-    },
-    { dispatch: false }
-  );
+  closeTechTypes$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(TechTypeActions.closeTechsTypes),
+      map(() => InicioActions.loadTechsTypes())
+    );
+  });
 
   openTechTypesForm$ = createEffect(
     () => {
@@ -49,7 +46,9 @@ export class TechTypeEffects {
         ofType(TechTypeActions.openTechsTypesForm),
         map((action) => {
           this.dialogService.open(TechTypeFormComponent, {
-            header: action.techType ? `Editar Tipo de Tecnología "${action.techType.name}"` : 'Agregar Tipo de Tecnología',
+            header: action.techType
+              ? `Editar Tipo de Tecnología "${action.techType.name}"`
+              : 'Agregar Tipo de Tecnología',
             data: { techType: action.techType },
           });
         })
@@ -75,10 +74,14 @@ export class TechTypeEffects {
   saveTechType$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TechTypeActions.saveTechsTypes),
-      mergeMap((action) => this.techService.saveTechType(action.techType).pipe(
-        map(() => TechTypeActions.saveTechsTypesSuccess()),
-        catchError((error) => of(TechTypeActions.saveTechsTypesFailure({error})))
-      ))
+      mergeMap((action) =>
+        this.techService.saveTechType(action.techType).pipe(
+          map(() => TechTypeActions.saveTechsTypesSuccess()),
+          catchError((error) =>
+            of(TechTypeActions.saveTechsTypesFailure({ error }))
+          )
+        )
+      )
     );
   });
 
@@ -87,35 +90,42 @@ export class TechTypeEffects {
       ofType(TechTypeActions.saveTechsTypesSuccess),
       map(() => {
         this.messageService.add({
-          severity: "success",
-          summary: "Éxito",
-          detail: "Tipo de Tecnología guardada con éxito."
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Tipo de Tecnología guardada con éxito.',
         });
         return TechTypeActions.loadTechsTypes({});
       })
     );
   });
 
-  saveTechFailure$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(TechTypeActions.saveTechsTypesFailure),
-      map(() => {
-        this.messageService.add({
-          severity: "error",
-          summary: "Error",
-          detail: "Error al guardar Tipo de Tecnología. Intente de nuevo."
-        });
-      })
-    );
-  }, { dispatch: false });
+  saveTechFailure$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(TechTypeActions.saveTechsTypesFailure),
+        map(() => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Error al guardar Tipo de Tecnología. Intente de nuevo.',
+          });
+        })
+      );
+    },
+    { dispatch: false }
+  );
 
   deleteTechType$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TechTypeActions.deleteTechsTypes),
-      mergeMap((action) => this.techService.deleteTechType(action.techType).pipe(
-        map(() => TechTypeActions.deleteTechsTypesSuccess()),
-        catchError((error) => of(TechTypeActions.deleteTechsTypesFailure({error})))
-      ))
+      mergeMap((action) =>
+        this.techService.deleteTechType(action.techType).pipe(
+          map(() => TechTypeActions.deleteTechsTypesSuccess()),
+          catchError((error) =>
+            of(TechTypeActions.deleteTechsTypesFailure({ error }))
+          )
+        )
+      )
     );
   });
 
@@ -124,25 +134,28 @@ export class TechTypeEffects {
       ofType(TechTypeActions.deleteTechsTypesSuccess),
       map(() => {
         this.messageService.add({
-          severity: "success",
-          summary: "Éxito",
-          detail: "Tipo de Tecnología eliminada con éxito."
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Tipo de Tecnología eliminada con éxito.',
         });
         return TechTypeActions.loadTechsTypes({});
       })
     );
   });
 
-  deleteTechFailure$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(TechTypeActions.deleteTechsTypesFailure),
-      map(() => {
-        this.messageService.add({
-          severity: "error",
-          summary: "Error",
-          detail: "Error al eliminar Tipo de Tecnología. Intente de nuevo."
-        });
-      })
-    );
-  }, { dispatch: false });
+  deleteTechFailure$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(TechTypeActions.deleteTechsTypesFailure),
+        map(() => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Error al eliminar Tipo de Tecnología. Intente de nuevo.',
+          });
+        })
+      );
+    },
+    { dispatch: false }
+  );
 }
--- src/app/modules/secured/tech-type-crud/tech-type-crud.component.ts
+++ src/app/modules/secured/tech-type-crud/tech-type-crud.component.ts
@@ -1,52 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { LazyLoadEvent } from 'primeng/api';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { TipoTecnologia } from 'src/app/project/models/tech.model';
 import { DynamicTableColumnModel } from 'src/core/classes/dynamic-table.model';
-import { selectTechTypes, selectTechTypesLoading } from './state/techs-type.selectors';
+import {
+  selectTechTypes,
+  selectTechTypesLoading,
+} from './state/techs-type.selectors';
 import * as techTypeActions from './state/techs-type.actions';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-tech-type-crud',
   templateUrl: './tech-type-crud.component.html',
-  styleUrls: ['./tech-type-crud.component.scss']
+  styleUrls: ['./tech-type-crud.component.scss'],
 })
-export class TechTypeCrudComponent implements OnInit {
-
+export class TechTypeCrudComponent implements OnInit, OnDestroy {
   cols: DynamicTableColumnModel[] = [
     {
-      header: "ID",
-      field: "id"
+      header: 'ID',
+      field: 'id',
     } as DynamicTableColumnModel,
     {
-      header: "Nombre",
-      field: "name"
+      header: 'Nombre',
+      field: 'name',
     } as DynamicTableColumnModel,
   ];
 
   values$!: Observable<TipoTecnologia[]>;
   valuesLoading$!: Observable<boolean>;
 
-  constructor(
-    private store: Store
-  ){}
+  onCloseSub$: Subscription = this.ref.onClose.subscribe(() =>
+    this.store.dispatch(techTypeActions.closeTechsTypes())
+  );
+
+  constructor(private store: Store, public ref: DynamicDialogRef) {}
 
   ngOnInit(): void {
     this.values$ = this.store.select(selectTechTypes);
     this.valuesLoading$ = this.store.select(selectTechTypesLoading);
   }
 
-  lazyLoadEvent(query: LazyLoadEvent){
-    this.store.dispatch(techTypeActions.loadTechsTypes({query}));
+  ngOnDestroy(): void {
+    this.onCloseSub$.unsubscribe();
   }
 
-  openForm(techType?: TipoTecnologia){
+  lazyLoadEvent(query: LazyLoadEvent) {
+    this.store.dispatch(techTypeActions.loadTechsTypes({ query }));
+  }
+
+  openForm(techType?: TipoTecnologia) {
     this.store.dispatch(techTypeActions.openTechsTypesForm({ techType }));
   }
 
-  delete(techType: TipoTecnologia){
-    this.store.dispatch(techTypeActions.deleteTechsTypes({techType}));
+  delete(techType: TipoTecnologia) {
+    this.store.dispatch(techTypeActions.deleteTechsTypes({ techType }));
   }
-
 }
