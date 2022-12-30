f9f3f288217d831ba9fa94169d44e7197a7e671a
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-30 08:51:08 -0300

    refactor: techtype.service

--- src/app/project/services/techtype.service.ts
+++ src/app/project/services/techtype.service.ts
@@ -1,31 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { EntityCollectionServiceBase, EntityCollectionServiceElementsFactory } from '@ngrx/data';
+import {
+  EntityCollectionServiceBase,
+  EntityCollectionServiceElementsFactory,
+} from '@ngrx/data';
 import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { MultipleRecordsResponse } from '../models/MultipleRecordsResponse';
 import { TechType } from '../models/TechType.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class TechtypeService extends EntityCollectionServiceBase<TechType>{
-
-  private apiUrl: string = environment.apiUrl;
-
-  constructor(serviceElementsFactory: EntityCollectionServiceElementsFactory, private http: HttpClient) {
+export class TechtypeService extends EntityCollectionServiceBase<TechType> {
+  constructor(
+    serviceElementsFactory: EntityCollectionServiceElementsFactory,
+    private http: HttpClient
+  ) {
     super('TechType', serviceElementsFactory);
   }
 
-  override getWithQuery(query: string): Observable<TechType[]> {
-      return this.http.get<MultipleRecordsResponse<TechType>>(`${this.apiUrl}/techtypes/`, {
-        params: {query}
-      }).pipe(map(value => {
-        this.totalRecords = value.totalRecords;
-        return value.data;
-      }));
-  }
-
+  private apiUrl: string = environment.apiUrl;
   totalRecords = 0;
-
+  
+  override getWithQuery(query: string): Observable<TechType[]> {
+    return this.http
+      .get<MultipleRecordsResponse<TechType>>(
+        `${this.apiUrl}/${this.entityName.toLowerCase()}s/`,
+        {
+          params: { query },
+        }
+      )
+      .pipe(
+        map((value) => {
+          this.totalRecords = value.totalRecords;
+          return value.data;
+        })
+      );
+  }
 }
