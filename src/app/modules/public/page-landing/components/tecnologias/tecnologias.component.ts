4144e6c8ddf5c0cb4652b48ec9fee7ded53ae10c
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-30 10:31:12 -0300

    fix: problemas con la lista de tecnologias

--- src/app/modules/public/page-landing/components/tecnologias/tecnologias.component.ts
+++ src/app/modules/public/page-landing/components/tecnologias/tecnologias.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { MenuItem } from 'primeng/api';
 import { map, Observable } from 'rxjs';
 import { Tecnologia } from 'src/app/project/models/tech.model';
 import { loadTechs } from '../../state/inicio.actions';
 import { openTechsTypes } from '../../../../secured/tech-type-crud/state/techs-type.actions';
-import { selectTechsInicio, selectTechTypesInicio } from '../../state/inicio.selectors';
+import {
+  selectTechsInicio,
+  selectTechTypesInicio,
+} from '../../state/inicio.selectors';
 import { selectTechCrudModalState } from '../../../../secured/tech-crud/state/tech.selectors';
 import { openTechsCrud } from 'src/app/modules/secured/tech-crud/state/tech.actions';
 import { TechType } from 'src/app/project/models/TechType.model';
-
 @Component({
   selector: 'app-tecnologias',
   templateUrl: './tecnologias.component.html',
@@ -26,11 +28,11 @@ export class TecnologiasComponent implements OnInit {
   ngOnInit(): void {
     this.techCrudModalVisible$ = this.store.select(selectTechCrudModalState);
     const response$ = this.store.select(selectTechTypesInicio);
-    this.techTypes$ = response$.pipe(
-      map(value => value.data)
-    )
+    this.techTypes$ = response$.pipe(map((value) => value.data));
     this.techs$ = this.store.select(selectTechsInicio);
   }
+
+  firstTime = true;
 
   techTypesToMenuItem(techTypes: TechType[]): MenuItem[] {
     return techTypes.map((techType) => ({ label: techType.name }));
@@ -44,7 +46,10 @@ export class TecnologiasComponent implements OnInit {
     this.store.dispatch(openTechsTypes());
   }
 
-  loadTechs(activeType: string) {
-    this.store.dispatch(loadTechs({ activeType }));
+  loadTechs(activeType: MenuItem) {
+    this.firstTime = false;
+    if (activeType.label) {
+      this.store.dispatch(loadTechs({ activeType: activeType.label }));
+    }
   }
 }
