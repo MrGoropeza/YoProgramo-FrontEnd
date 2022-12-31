455fc42d259ce332d02ac4122f34f727e1df5d61
Gonzalo Oropeza <goropeza8@gmail.com>
2022-12-31 09:40:33 -0300

    add: parametro para deshabilitar boton

--- src/app/components/ui-button/ui-button.component.ts
+++ src/app/components/ui-button/ui-button.component.ts
@@ -1,23 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-ui-button',
   templateUrl: './ui-button.component.html',
   styleUrls: ['./ui-button.component.scss']
 })
-export class UiButtonComponent implements OnInit {
+export class UiButtonComponent {
 
   @Input() label!: string;
   @Input() icon!: string;
   @Input() class!: string;
 
   @Input() loading!: boolean;
+  @Input() disabled = false;
 
   @Output() clickAction = new EventEmitter();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
