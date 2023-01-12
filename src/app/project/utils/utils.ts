701d5b8dfc1d9547d76ed874db6531f275e4f68e
Gonzalo Oropeza <goropeza8@gmail.com>
2023-01-12 12:42:15 -0300

    add: util para transformar lista de techs en SelectItemGroup[]

---
+++ src/app/project/utils/utils.ts
@@ -0,0 +1,20 @@
+import { SelectItem, SelectItemGroup } from 'primeng/api';
+import { Tech } from '../models/Tech.model';
+import { TechType } from '../models/TechType.model';
+
+export const getSelectItemGroup = (techsList: Tech[]): SelectItemGroup[] => {
+  let techTypes: TechType[] = techsList
+    .map((response) => response.tipo)
+    .filter(
+      (techType, index, self) =>
+        self.findIndex((value) => value.id === techType.id) === index
+    );
+
+  return techTypes.map((techType) => ({
+    label: techType.name,
+    value: techType,
+    items: techsList
+      .filter((tech) => tech.tipo.id === techType.id)
+      .map((tech) => ({ label: tech.name, value: tech } as SelectItem<Tech>)),
+  }));
+};
