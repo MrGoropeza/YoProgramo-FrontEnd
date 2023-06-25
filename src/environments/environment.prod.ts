567d43bd9e8b3544a26d7038dc23950f3d0b034c
Gonzalo Oropeza <goropeza8@gmail.com>
2023-06-25 17:56:31 -0300

    fix: missing https -> apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'yoprogramo-backend-production-a632.up.railway.app',
+  apiUrl: 'https://yoprogramo-backend-production-a632.up.railway.app/',
 };
